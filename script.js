e7ba4469d30260b01842dc4ef2c051c5ac6b96e6
Jia Hao Choo <71251813+jiah29@users.noreply.github.com>
2024-02-29 00:51:08 -0500

    improve documentation

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 // ============================================================================
 // This script is used for index.html and suggestions.html.
-// Some functions are from Bootstrap v5.0 documentation to support
-// Bootstrap-specific features, such as popovers.
-// Reference: https://getbootstrap.com/docs/5.0/
-// Created by Jia Hao Choo for GGR472 Lab 2 (Winter 2024)
+// Created by Jia Hao Choo for GGR472 Lab 3 (Winter 2024)
 // ============================================================================
 
 /* ----------------------------------------------------------------------------
